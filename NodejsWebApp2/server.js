f0a4750d392397b772ca67993426c8a9e5b51ac8
bjiake <suyd@list.ru>
2023-02-07 23:14:45 +0700

    Добавьте файлы проекта.

---
+++ NodejsWebApp2/server.js
@@ -0,0 +1,10 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8 '});
+    const stream = fs.createReadStream('./index.html')
+    stream.pipe(res)
+}).listen(port);

f9471efa28c7d0714939e286a1d1ac4be586caa6
bjiake <suyd@list.ru>
2023-02-09 15:33:58 +0700

    10.1

--- NodejsWebApp2/server.js
+++ NodejsWebApp2/server.js
@@ -2,44 +2,7 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 const fs = require('fs');
-let student = {
-    FirstName: "Tom",
-    LastName: "Jhonson",
-    Otchestvo: "Otch",
-    Bithsday: 2003,
-    class: 5,
-    lessons: ["Math", "Language", "QualityMath"],
-    srokObucheniya: 9,
-    upClass: function () {
-        if (this.class > this.srokObucheniya) {
-            alert("Максимальный класс достигнут");
-        }
-        else {
-            this.class = this.class + 1;
-        }
-    },
-    Display: function () {
-        /*
-        console.log(student.lessons);
-        console.log(student.class);
-        console.log(student.LastName);
-        console.log(student.propiska.city);
-        console.log(student.projivanie.region);
-        */
-        alert(JSON.stringify(student));
-    },
-    propiska: {
-        country: "russia",
-        city: "kemerovo",
-        idHoume: "54",
-        idKvarti: "545"
-    },
-    projivanie: {
-        region: "tyva",
-        punct: "kyzyl",
-        street: "Moskow 44"
-    }
-}
+var newStudent = new student();
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8 '});
